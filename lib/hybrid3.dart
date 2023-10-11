e35d232ce2516dcdee08067e12efdc3a1ff13214
Teagan Clayton <clay0063@algonquinlive.com>
2023-10-11 16:46:01 -0400

    Mild formatting adjustments

--- lib/hybrid3.dart
+++ lib/hybrid3.dart
@@ -3,7 +3,7 @@ class Student {
   Student(this.people);
 
   void sortList(String field) {
-    var keyCheck = people.every((person)=>person[field] != null); //checks and sees if the field exists for the items
+    var keyCheck = people.every((person) => person[field] != null); //checks and sees if the field exists for the items
     if (!keyCheck) { field = "first"; } //if it doesn't exist, default to sorting by first name
     return people.sort((a, b) => a[field]!.compareTo(b[field]!));
   }
@@ -13,7 +13,9 @@ class Student {
   }
 
   void plus(Map<String, String> person) {
-    if (person.containsKey("first") && person.containsKey("last") && person.containsKey("email")) {
+    if (person.containsKey("first") &&
+        person.containsKey("last") &&
+        person.containsKey("email")) {
       return people.add(person);
     } else {
       print("New information requires First name, Last name, and Email");
@@ -21,6 +23,7 @@ class Student {
   }
 
   void remove(String field) {
-    return people.removeWhere((person) => person.containsValue(field)); //does not cause errors if something doesnt exist
+    return people.removeWhere((person) => person.containsValue(
+        field)); //does not cause errors if something doesn't exist
   }
 }
